6cbeda2f37636ae8d81688773fb2967810375c4c
maya.h <kushirajput707@gmail.com>
2026-02-14 14:07:01 +0530

    Updated portfolio version

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function toggleMode() {
     document.body.classList.toggle("dark-mode");
 }
+
+const text = ["Web Developer", "Tech Enthusiast", "Future Software Engineer"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+    if (count === text.length) {
+        count = 0;
+    }
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 100);
+})();
